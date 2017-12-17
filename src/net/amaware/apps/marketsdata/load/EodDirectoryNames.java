afbbcedde6550a6b806ceda287307a6e1279696e
angelo <angelo@amaware.net>
2017-12-17 09:57:39 -0500

    a

---
+++ src/net/amaware/apps/marketsdata/load/EodDirectoryNames.java
@@ -0,0 +1,93 @@
+/**
+ * 
+ */
+package net.amaware.apps.marketsdata.load;
+
+import java.io.File;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+import net.amaware.apps.marketsdata.load.MainMarketDataLoads.DirectoryName;
+import net.amaware.appsbase.datatrack.DataTrackAccess;
+import net.amaware.autil.ACommDb;
+import net.amaware.autil.ACommDbFile;
+import net.amaware.autil.ADataColResult;
+import net.amaware.autil.AException;
+import net.amaware.autil.AExceptionSql;
+import net.amaware.autil.AFileO;
+import net.amaware.serv.HtmlTargetServ;
+import net.amaware.serv.SourceProperty;
+
+
+/**
+ * @author PSDAA88 - Angelo M Adduci - Sep 6, 2005 3:02:12 PM
+ * 
+ */
+
+public class EodDirectoryNames extends DataTrackAccess  {
+/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	final String thisClassName = this.getClass().getName();	
+	//
+
+	/**
+	 * 
+	 */
+	public EodDirectoryNames(ACommDb acomm, String ipropFileName) {
+		//                 subject, topic, item, description
+		super(acomm, ipropFileName);
+		//set file attributes
+
+	}
+
+	public boolean doProcess(ACommDbFile acomm) {
+		
+		doDirectoryFileProcess(acomm, Arrays.asList(".txt"));
+		
+		return true;
+	}	
+	
+	public void doFileFound(ACommDbFile acomm, String _fileFullName) {
+		//acomm.addPageMsgsLineOut(thisClassName+"=>Input File{" + _fileFullName +"}");
+		
+		acomm.addPageMsgsLineOut(" ");
+		String filenamename=acomm.getFileNameName(_fileFullName);
+    	switch (filenamename.toLowerCase()) {
+          case "amex.txt": 
+			   acomm.addPageMsgsLineOut(thisClassName+" NO Processing for{"+filenamename+"}");
+   	       
+               break;
+
+          case "exchanges.txt": 
+        	   acomm.addPageMsgsLineOut(thisClassName+" Processing for{"+filenamename+"}");
+        	   
+        	   doFileProcessed(acomm,_fileFullName);
+        	  
+   	           //
+               break;
+           
+        default: 
+	    	  //processFile(acomm, inFile);
+	    	   String outmsg=thisClassName+"===UNKNOWN Process for File  Name{"+filenamename+"}";
+			   acomm.addPageMsgsLineOut(outmsg);
+	    	  //throw new AException(acomm, outmsg);	    	
+  	    }
+		
+	}
+	
+	public void doFilesEnded(ACommDbFile acomm, int _filesFound) {
+		//acomm.addPageMsgsLineOut(thisClassName+"=>No Method Override for Input File{" + _fileFullName +"}");
+	}	
+	//
+	// END
+	//	
+ }
--- src/net/amaware/apps/marketsdata/load/MainMarketDataLoads.java
+++ src/net/amaware/apps/marketsdata/load/MainMarketDataLoads.java
@@ -16,7 +16,7 @@ import net.amaware.serv.HtmlTargetServ;
 import net.amaware.serv.SourceProperty;
 import net.amaware.app.DataStoreReport;
 import net.amaware.app.MainAppDataStore;
-
+import net.amaware.appsbase.datatrack.DataTrackAccess;
 import net.amaware.autil.*;
 //
 /**
@@ -26,8 +26,8 @@ import net.amaware.autil.*;
 public class MainMarketDataLoads {
 	final static String thisClassName = "MainMarketDataLoads";	
 	// set Properties file to being used
-    final static String propFileName            = "MainMarketDataLoads.properties";
-
+    final static String thisPropFileName      = "MainMarketDataLoads.properties";
+    final static String marketsPropFileName   = "markets-amaware.properties";
     //
     
     //
@@ -76,62 +76,40 @@ public class MainMarketDataLoads {
 		//
 		
 		try { //setup the com class with properties file and log file prop key
-			acomm = new ACommDbFile(propFileName, args);
+			acomm = new ACommDbFile(thisPropFileName, args);
 			
-			String dirLastName="";
-			
-			acomm.addPageMsgsLineOut(thisClassName+"=>getArgFilePath{" + acomm.getArgFilePath() +"}"
-					                              +" |ArgFilePathDirName{" + acomm.getArgFilePathDirName() +"}"
+		
+			acomm.addPageMsgsLineOut(thisClassName+"=>getArgFilePath{" + ACommDbFile.getArgFilePath() +"}"
+					                              +" |ArgFilePathDirName{" + ACommDbFile.getArgFilePathDirName() +"}"
 					                              );			
 			
-			if (AComm.isArgFileDirectory()) {
-			
-				acomm.getArgFilePath();
-				
-				
-			}
-			
-  		    //for (String thisFileName: acomm.getFileList(Arrays.asList(".xls",".xlst",".txt"))) {
-			for (String thisFileName: acomm.getFileList(Arrays.asList("*.*"))) {
-  		    	   acomm.addPageMsgsLineOut(thisClassName+"=>Input File{" + thisFileName +"}");
-  		    	   //
-					int colHeadRow=2;
-					int colDetRow=3;
-					//
-   
-  		    	   
-  		    	   //
-					
-					acomm.addPageMsgsLineOut(thisClassName+"==>File ColHeaderRow{" + colHeadRow +"}"
-							                +" |ColDetailRowStart{" + colDetRow +"}");
-					/*
-  		    	    ExcelDbTable aExcelDbTable = new ExcelDbTable();
-  		    	    
-					mainApp = new MainAppDataStore(acomm, aExcelDbTable, thisFileName, acomm.getFileTextDelimTab());
-  		    	    //mainApp = new MainAppDataStore(acomm, new ExcelDbTable(), thisFileName, '|');
-					//
-					mainApp.setSourceHeadRowStart(1);
+	    	switch (ACommDbFile.getArgFilePathDirName().toLowerCase()) {
+              case "fundamentals": 
+				   acomm.addPageMsgsLineOut("Processing for{"+ACommDbFile.getArgFilePathDirName()+"}");
+         	       directoryName = DirectoryName.FUNDAMENTALS;
+                   break;
 
-					//
-					mainApp.setSourceDataHeadRowStart(colHeadRow);
-					mainApp.setSourceDataRowStart(colDetRow);
-					//
-					//mainApp.setSourceDataRowEnd(10);
-					
-					aExcelDbTable.setMainAppDataStore(mainApp);
-					
-					//
-					mainApp.doProcess(acomm, "MainExcelDbTable");
-					*/		
-  		    }
-  		    
-			//mainApp = new MainAppDataStore(acomm, _fileProcess, args, acomm.getFileTextEndLine(), 3);
-			//mainApp = new MainAppDataStore(acomm, _fileProcess, args, ' ');
-			//mainApp = new MainAppDataStore(acomm, _fileProcess, args, acomm.getFileTextDelimTab());
-			
-			//mainApp.getHtmlServ().outPageLine(acomm, thisClassName+" completed ");
-			acomm.end();
-			
+              case "names": 
+            	   acomm.addPageMsgsLineOut("Processing for{"+ACommDbFile.getArgFilePathDirName()+"}");
+         	       directoryName = DirectoryName.NAMES;
+         	    
+         	       EodDirectoryNames aEodDirectoryNames = new EodDirectoryNames(acomm, marketsPropFileName);
+         	       aEodDirectoryNames.doProcess(acomm);
+         	       //
+         	       aEodDirectoryNames.connectionEnd();
+         	       //
+                   break;
+                 
+              default: 
+		    	  //processFile(acomm, inFile);
+		    	   String outmsg="===UNKNOWN Directory Name===>"+ACommDbFile.getArgFilePathDirName();
+				   acomm.addPageMsgsLineOut(thisClassName+"===UNKNOWN Directory Name===>"+ACommDbFile.getArgFilePathDirName());
+		    	  throw new AException(acomm, outmsg);	    	
+	    	}
+	    	//
+	    	acomm.dbConClose();
+	    	acomm.end();
+			//
 		} catch (AException e1) {
 			acomm.addPageMsgsLineOut("MainExcelDbLoad AException msg{"+e1.getMessage()+"}");
 			throw e1;
@@ -139,234 +117,7 @@ public class MainMarketDataLoads {
 
 	}	
 	//
-	public static void mainold(String[] args) {
-		//
-		String dirName="";
-		String dirPath="";
-		try {  
-			acomm = new ACommDb(propFileName);
-		    //String inFileName 
-		    File inFile = inputArgsSql(acomm,args);
-		    
-	    	aHtmlServReport = new HtmlTargetServ(acomm
-				    , inFile.getPath() //inFile.getPath() 
-				      + ".html"
-				    , thisClassName + " for " + acomm.getUserName()
-				    //, getReportStyleRow()
-				    //, getReportStyleRowCol()
-				    
-				    //, optList
-				    );		    	
-		    
-	    	//aHtmlServReport.outTargetLine(acomm, thisClassName,"");
-	    	//aHtmlServReport.echo("<table>");	    	
-	    	
-	    	Vector<String> aVector = new Vector<String>();
-	    	aVector.addElement( new String (thisClassName));
-	    	
-		    if (inFile.isDirectory()) {
-		    	dirPath=inFile.getPath();
-		    	dirName=inFile.getName();
-		    	
-		    	//DirectoryName =dirName;
-		    	
-		    	aVector.addElement(dirPath);		    	
-		    	aHtmlServReport.outTargetHeading(acomm, aVector);
 
-		    	switch (dirName.toLowerCase()) {
-	               case "fundamentals": 
-	            	    directoryName = DirectoryName.FUNDAMENTALS;
-	                    break;
-
-	               case "names": 
-	            	    directoryName = DirectoryName.NAMES;
-	                    break;
-	                    
-	               default: 
-	   		    	//processFile(acomm, inFile);
-	   		    	String outmsg="===UNKNOWN Directory Name===>"+inFile.getName();
-	   		    	
-	   		    	aHtmlServReport.outTargetLine(acomm, thisClassName+"=>"+outmsg);
-	   		    	
-	   		    	throw new AException(acomm, outmsg);	    	
-	                    
-		    	}
-               				
-		    	 File[] listOfFiles = inFile.listFiles();
-		    	 for (File file : listOfFiles) {
-		    	    if (file.isFile()) {
-		    	    	if (file.getName().endsWith(".txt")){
-		    	    	    if (file.getName().endsWith("readme.txt")) {
-		    	    	    	aHtmlServReport.outTargetLine(acomm, "File bypassed...=>"+file.getName(),"color:orange;border:solid orange .1em;");
-		    	    	    	
-		    	    	    } else if (file.getName().endsWith("report.txt")) {		    	    	    	
-		    	    	    	aHtmlServReport.outTargetLine(acomm, "File bypassed...=>"+file.getName(),"color:orange;border:solid orange .1em;");
-		    	    	    	
-		    	    	    } else {
-		    	    	    	 try {	    	    	
-                                 	switch (directoryName) {
-									  case FUNDAMENTALS:
-										/*   
-										   thisDataStore = eodFundamentalsDirFile = new EodFundamentalsDirFile();
-										   
-			    	    	    	       mainApp = new MainAppDataStore(acomm, thisDataStore, file.getPath(), acomm.getFileTextDelimTab());
-			    	    	    	       mainApp.setSourceHeadRowStart(0);
-			    					       mainApp.setSourceDataHeadRowStart(1);
-			    					       mainApp.setSourceDataHeadRowEnd(1);
-			    					       mainApp.setSourceDataRowStart(2);
-			    					       mainApp.doProcess(acomm, thisClassName);
-			    					       
-			    					       if (eodFundamentalsDirFile.fileProcessStatus == FileProcessStatus.Processed) {
-			    					    	   aHtmlServReport.outTargetLine(acomm, "Completed{"+file.getPath()+"}","color:green;");
-			    					       } else {
-			    					           aHtmlServReport.outTargetLine(acomm, "Not Processed Status returned for file{"+file.getPath()+"}"
-			    					        		   + ".....Status{" + eodFundamentalsDirFile.fileProcessStatus +  "}"
-			    					        		   ,"color:red;font-weight:bold;");
-			    					       }
-										   
-										  */ 
-										   break;
-											
-									  case NAMES:
-	                                       aHtmlServReport.outTargetLine(acomm, "Processing{"+file.getPath()+"}" 
-									                                     //+ " Name{"+file.getName()+"}"
-	                                    		                         ,"color:blue;border:solid blue .2em;");
-	                                       
-	                                       fileNameToProcess="exchanges.txt";
-										   if (file.getName().toLowerCase().contentEquals(fileNameToProcess)){
-											   try {
-												   
-											       thisDataStore = new EodNamesDirExchangeTxt(acomm, file);
-											       
-											       //thisDataStore.setFileProcessStatus(FileProcessStatus.Reload);
-											       
-				    					           thisDataStore.doProcess(acomm, thisClassName, aHtmlServReport);
-
-				    					           if (thisDataStore.getFileProcessStatus() == FileProcessStatus.Processed) {
-				    					           } else {
-				    					           }
-				    					           
-											   } catch (AException e) {
-				    					    	   //aHtmlServReport.outTargetLine(acomm, "EXCEPTION{"+e.getMessage()+"}"  
-					    			               //          ,"border-bottom:solid red .2em;color:maroon;font-weight:bold;");
-												 throw e;
-											   } 
-											   
-										   } else {
-		                                 		aHtmlServReport.outTargetLine(acomm, "Bypassed Unknown File Name {"+file.getName()+"}"+ " Expecting{"+fileNameToProcess+"}","color:orange;");
-										   }
-
-										   break;
-										   
-										default:
-											 break;
-										}
-		    	    	    		 
-		    	   			  } catch (AException e) {
-		    	   				  
-					    	         aHtmlServReport.outTargetLine(acomm
-   					    	        		 , "AEXCEPTION Msg{"+e.getMessage()+"}" 
-  			                                 ,"border-top:solid red .2em;color:maroon;font-weight:bold;");
-   					    	         
-   					    	         aHtmlServReport.outTargetLine(acomm
-   					    	        		 , " ExpMsg{"+e.getExceptionMsg()+"}" 
-  			                                 ,"border-bottom:solid red 0em;color:maroon;font-weight:bold;");
-   					    	         
-   					    	         aHtmlServReport.outTargetLine(acomm
-   					    	        		 , " Code{"+e.getExceptionCode()+"}" + " State{"+e.getExceptionState()+"}"  
-  			                                 ,"border-bottom:solid red .2em;color:maroon;font-weight:bold;");
-   					    	         
-		    					     throw e;
-		    					  
-		    	   			  } catch (Exception e) {
-				    	         aHtmlServReport.outTargetLine(acomm, "EXCEPTION{"+e.getMessage()+"}"  
-		  			                         ,"border-bottom:solid red .2em;color:maroon;font-weight:bold;");
-		    	   				  
-				    	           throw e;
-		    					  //throw new AException(acomm,"Msg{"+e.getMessage());
-		    					  
-		    				  }
-
-		    	    	    }
-			    	        
-		    	    	} else {		    	    		
-		    	    		aHtmlServReport.outTargetLine(acomm, "File bypassed...not .txt=>"+file.getPath(),"color:orange;border:solid orange .1em;");	
-		    	    	}
-		    	    }
-		    	}
-		    	
-		    	
-		    } else {
-		    	aHtmlServReport.outTargetHeading(acomm, aVector);
-		    	
-		    	//processFile(acomm, inFile);
-		    	String outmsg="===NOT a Directory Name===>"+inFile.getName();
-		    	
-		    	aHtmlServReport.outTargetLine(acomm, thisClassName+"=>"+outmsg);
-		    	throw new AException(acomm, outmsg);	    	
-		    }
-
-		    aHtmlServReport.outTargetLine(acomm, thisClassName+" completed ","text-align:center;color:purple;");
-			//mainApp.getHtmlServ().outPageLine(acomm, thisClassName+" completed ");
-		    //
-		    aHtmlServReport.outTargetTrailer(acomm, aHtmlServReport.getDirFileName());
-		    
-		    //aHtmlServReport.echo("</body>");
-		    
-			acomm.end();
-			
-		} catch (AException e1) {
-			throw e1;
-		}
-
-	}
-
-	
-	   public static File inputArgsSql(ACommDb acomm, String[] args) {
-			
-			/**/
-			String currentDirectory;
-			File file = new File(".");
-			currentDirectory = file.getAbsolutePath();
-			System.out.println("Initial working directory : "+currentDirectory);
-			/**/		   
-		   
-		    System.out.println("====Args Input Process==========");
-		    String filename="";
-		    if (args.length == 0) {        
-		    	System.out.println("========None==========");
-		    	
-			    	   throw new AException(acomm
-				    		, "No input arguments...FileName to process is required. Must beging with tablename");
-		    	
-		    } else {
-		    	System.out.println("--Args Input---len=" + args.length);	    	
-		    	for (int i = 0; i < args.length; i++) {
-			    	System.out.println(i + ">" + args[i]);    			
-		    	}
-				if (args[0].length() == 0) {
-				    	throw new AException(acomm
-					    		, "===Arg FileName needed======");	    	
-				}
-				
-				filename=args[0];
-
-			}
-			acomm.addPageMsgsLineOut("_______________Input Args_________________");
-			for (int i=0; i < args.length;i++){
-		    	acomm.addPageMsgsLineOut("Arg#"+i+"=" + args[i]);
-			}
-			//
-			File f = new File(filename);
-			filename=f.getName();			
-			//
-			file = new File(".");
-			currentDirectory = file.getAbsolutePath();
-			System.out.println("Current working directory : "+currentDirectory);
-			//
-			return f;
-			
-		}
 //
 // END CLASS
 //	
